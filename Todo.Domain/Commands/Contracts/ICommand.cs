f141898c04a53082f51dde9e5d4266c310b02368
Natanael <natanaelsantosbr@gmail.com>
2021-10-21 20:35:01 -0300

    Create Command

--- Todo.Domain/Commands/Contracts/ICommand.cs
+++ Todo.Domain/Commands/Contracts/ICommand.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Todo.Domain.Commands.Contracts
+﻿namespace Todo.Domain.Commands.Contracts
 {
-    public interface ICommand 
+    public interface ICommand
     {
         bool Validate();
     }
