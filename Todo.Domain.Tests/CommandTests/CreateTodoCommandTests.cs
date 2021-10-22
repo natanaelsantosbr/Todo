71724ee0815d9345e4b3d3e26fc875a4f3561626
Natanael <natanaelsantosbr@gmail.com>
2021-10-21 21:35:38 -0300

    Refatorando Tests

--- Todo.Domain.Tests/CommandTests/CreateTodoCommandTests.cs
+++ Todo.Domain.Tests/CommandTests/CreateTodoCommandTests.cs
@@ -7,23 +7,27 @@ namespace Todo.Domain.Tests.CommandTests
     [TestClass]
     public class CreateTodoCommandTests
     {
+        private readonly CreateTodoCommand _invalidCommand = new CreateTodoCommand("", "", DateTime.Now);
+
+        private readonly CreateTodoCommand _validCommand = new CreateTodoCommand("Titulo da Tarefa", "Natanael", DateTime.Now);
+
+        public CreateTodoCommandTests()
+        {
+            _invalidCommand.Validate();
+            _validCommand.Validate();
+        }
+
         [TestMethod]
         public void Dado_um_comando_invalido()
-        {
-            var command = new CreateTodoCommand("", "", DateTime.Now);
-            command.Validate();
-            Assert.AreEqual(command.Valid, false);
-            Assert.AreEqual(command.Invalid, true);
+        {            
+            Assert.AreEqual(_invalidCommand.Valid, false);
         }
 
 
         [TestMethod]
         public void Dado_um_comando_valido()
-        {
-            var command = new CreateTodoCommand("Titulo da Tarefa", "Natanael", DateTime.Now);
-            command.Validate();
-            Assert.AreEqual(command.Valid, true);
-            Assert.AreEqual(command.Invalid, false);
+        {            
+            Assert.AreEqual(_validCommand.Valid, true);
         }
     }
 }
