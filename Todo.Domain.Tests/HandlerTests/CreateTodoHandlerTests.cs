c7a53bf58fdb1e2e276d67a2d479c5934c4ca716
Natanael <natanaelsantosbr@gmail.com>
2021-10-27 20:46:33 -0300

    Fake Repositorio

---
+++ Todo.Domain.Tests/HandlerTests/CreateTodoHandlerTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Todo.Domain.Commands;
+using Todo.Domain.Handlers;
+using Todo.Domain.Tests.Repositories;
+
+namespace Todo.Domain.Tests.HandlerTests
+{
+    [TestClass]
+    public class CreateTodoHandlerTests
+    {
+        [TestMethod]
+        public void Dado_um_comando_invalido_deve_interromper_a_execucao()
+        {
+            var command = new CreateTodoCommand("", "", DateTime.Now);
+            var handler = new TodoHandler(new FakeTodoRepository());
+        }
+
+        [TestMethod]
+        public void Dado_um_comando_valido_deve_criar_a_tarefa()
+        {
+
+        }
+    }
+}
---
+++ Todo.Domain.Tests/Repositories/FakeTodoRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Todo.Domain.Entities;
+using Todo.Domain.Repositories;
+
+namespace Todo.Domain.Tests.Repositories
+{
+    public class FakeTodoRepository : ITodoRepository
+    {
+        public void Create(TodoItem todo)
+        {
+
+        }
+
+        public void Update(TodoItem todo)
+        {
+
+        }
+    }
+}
