a49aedc9d27e733fa0457d310eb736d58cc43b15
Natanael <natanaelsantosbr@gmail.com>
2021-10-27 21:50:38 -0300

    Testando as Queries

---
+++ Todo.Domain.Tests/QueriesTests/TodoQueryTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Todo.Domain.Entities;
+using Todo.Domain.Queries;
+
+namespace Todo.Domain.Tests.QueriesTests
+{
+    [TestClass]
+    public class TodoQueryTests
+    {
+        private List<TodoItem> _items;
+
+        public TodoQueryTests()
+        {
+            _items = new List<TodoItem>();
+            _items.Add(new TodoItem("Tarefa 1", DateTime.Now, "natanael"));
+            _items.Add(new TodoItem("Tarefa 2", DateTime.Now, "natanael2"));
+            _items.Add(new TodoItem("Tarefa 3", DateTime.Now, "natanael"));
+            _items.Add(new TodoItem("Tarefa 4", DateTime.Now, "natanael2"));
+            _items.Add(new TodoItem("Tarefa 5", DateTime.Now, "natanael"));
+
+        }
+
+
+        [TestMethod]
+        public void Dada_a_consulta_deve_retornar_tarefas_apenas_do_usuario_natanael()
+        {
+            var result = _items.AsQueryable().Where(TodoQueries.GetAll("natanael"));
+            Assert.AreEqual(3, result.Count());
+        }
+    }
+}
